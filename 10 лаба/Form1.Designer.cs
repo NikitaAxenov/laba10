a18d16cb02f8bc5ba9b140b2f8a0533199682dc7
NikitaAxenov <64533263+NikitaAxenov@users.noreply.github.com>
2021-05-16 22:31:55 +0300

    Сделано

---
+++ 10 лаба/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace _10_лаба
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.one = new System.Windows.Forms.Label();
+            this.seven = new System.Windows.Forms.Label();
+            this.five = new System.Windows.Forms.Label();
+            this.three = new System.Windows.Forms.Label();
+            this.four = new System.Windows.Forms.Label();
+            this.two = new System.Windows.Forms.Label();
+            this.six = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // one
+            // 
+            this.one.AutoSize = true;
+            this.one.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.one.Location = new System.Drawing.Point(586, 197);
+            this.one.Name = "one";
+            this.one.Size = new System.Drawing.Size(0, 29);
+            this.one.TabIndex = 0;
+            this.one.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // seven
+            // 
+            this.seven.AutoSize = true;
+            this.seven.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.seven.Location = new System.Drawing.Point(635, 256);
+            this.seven.Name = "seven";
+            this.seven.Size = new System.Drawing.Size(0, 29);
+            this.seven.TabIndex = 1;
+            // 
+            // five
+            // 
+            this.five.AutoSize = true;
+            this.five.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.five.Location = new System.Drawing.Point(635, 227);
+            this.five.Name = "five";
+            this.five.Size = new System.Drawing.Size(0, 29);
+            this.five.TabIndex = 2;
+            // 
+            // three
+            // 
+            this.three.AutoSize = true;
+            this.three.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.three.Location = new System.Drawing.Point(586, 227);
+            this.three.Name = "three";
+            this.three.Size = new System.Drawing.Size(0, 29);
+            this.three.TabIndex = 3;
+            // 
+            // four
+            // 
+            this.four.AutoSize = true;
+            this.four.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.four.Location = new System.Drawing.Point(610, 227);
+            this.four.Name = "four";
+            this.four.Size = new System.Drawing.Size(0, 29);
+            this.four.TabIndex = 4;
+            // 
+            // two
+            // 
+            this.two.AutoSize = true;
+            this.two.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.two.Location = new System.Drawing.Point(635, 197);
+            this.two.Name = "two";
+            this.two.Size = new System.Drawing.Size(0, 29);
+            this.two.TabIndex = 5;
+            // 
+            // six
+            // 
+            this.six.AutoSize = true;
+            this.six.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.six.Location = new System.Drawing.Point(586, 256);
+            this.six.Name = "six";
+            this.six.Size = new System.Drawing.Size(0, 29);
+            this.six.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(528, 122);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(186, 49);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Кинуть Кубик";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1371, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.six);
+            this.Controls.Add(this.two);
+            this.Controls.Add(this.four);
+            this.Controls.Add(this.three);
+            this.Controls.Add(this.five);
+            this.Controls.Add(this.seven);
+            this.Controls.Add(this.one);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label one;
+        private System.Windows.Forms.Label seven;
+        private System.Windows.Forms.Label five;
+        private System.Windows.Forms.Label three;
+        private System.Windows.Forms.Label four;
+        private System.Windows.Forms.Label two;
+        private System.Windows.Forms.Label six;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ 10 лаба/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _10_лаба
+{
+    public partial class Form1 : Form
+    {
+        const double b = 4294967299;
+        const double m = 9223372036854775808;
+        double xNext = b;
+        double xBefore, xNow;
+        public Form1()
+        {
+            InitializeComponent();
+            for (int i = 0; i < 20; i++)
+            {
+                xBefore = xNext;
+                xNext = (b * xBefore) % m;
+                xNow = xNext / m;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            one.Text = "";
+            two.Text = "";
+            three.Text = "";
+            four.Text = "";
+            five.Text = "";
+            six.Text = "";
+            seven.Text = "";
+            xBefore = xNext;
+            xNext = (b * xBefore) % m;
+            xNow = xNext / m;
+            if (xNow < 0.15)
+                four.Text = "•";
+            else if (xNow >= 0.15 && xNow < 0.3)
+            {
+                one.Text = "•";
+                seven.Text = "•";
+            }
+            else if (xNow >= 0.3 && xNow < 0.45)
+            {
+                one.Text = "•";
+                four.Text = "•";
+                seven.Text = "•";
+            }
+            else if (xNow >= 0.45 && xNow < 0.6)
+            {
+                one.Text = "•";
+                two.Text = "•";
+                six.Text = "•";
+                seven.Text = "•";
+            }
+            else if (xNow >= 0.6 && xNow < 0.85)
+            {
+                one.Text = "•";
+                two.Text = "•";
+                four.Text = "•";
+                six.Text = "•";
+                seven.Text = "•";
+            }
+            else if (xNow >= 0.85 && xNow < 1)
+            {
+                one.Text = "•";
+                two.Text = "•";
+                three.Text = "•";
+                five.Text = "•";
+                six.Text = "•";
+                seven.Text = "•";
+            }
+        }
+    }
+}
